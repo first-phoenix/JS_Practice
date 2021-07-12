313a8e17ba5bb926fbbc8ae381b392614d1188d7
first-phoenix <iswarajkhan@gmail.com>
2021-07-13 00:26:36 +0530

    New variable declaration using let and  const and end of second video

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 //Old Method to declare variables
 
-var life = 100; // this is the life bar
+// var life = 100; // this is the life bar
 
-life = 50;
+// life = 50;
 
-var name = 'Swaraj'; // this is the name
+// var name = 'Swaraj'; // this is the name
 
-var checkout = true; // a boolean
+// var checkout = true; // a boolean
 
-var box; // undefined
+// var box; // undefined
 
-var abc = null; // null value
+// var abc = null; // null value
 
-object, symbol
+// object, symbol
+
+
 
 
 // New way to create variable using let and const
 
+// const life = 100; // a const
+// life = 50; // it will give error as life is a const
+
+// let life = 100; // a let which value can be changed
+// life = 50; // it will not give error as it is a let
 
 
-
-console.log(box);
+console.log(life);

ffdbc2d0ba6dc7162b9a577b2acb50c0b328cdce
first-phoenix <iswarajkhan@gmail.com>
2021-07-13 00:54:04 +0530

    End of third video, discussed about the declaration of function

--- app.js
+++ app.js
@@ -26,4 +26,42 @@
 // life = 50; // it will not give error as it is a let
 
 
-console.log(life);
+// console.log(life);
+
+
+
+
+
+
+//Functions
+const name = "Swaraj";
+const youtuber = "hello world";
+
+// function logger() { // sample function
+//     console.log('Party Time'); //shift+alt+ down arrow to copy like this
+//     console.log('Party Time');
+//     console.log('Party Time');
+//     console.log('Party Time');
+// }
+
+// logger(); // call the function
+
+
+function upperCase(text) {
+    // this is the first way to declare function, old way
+    const uppercased = text.toUpperCase();
+    console.log(uppercased);
+}
+
+const upperCase1 = function() {
+    //write function here
+    //this is the second way to declare function
+}
+
+const upperCase2 = () => {
+    //write function here
+    //this is the third way ti declare function, new way
+}
+
+upperCase(name);
+upperCase(youtuber);

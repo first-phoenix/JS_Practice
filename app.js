4dc9ab42657a0f011be644db84bb0550a2aabb66
first-phoenix <iswarajkhan@gmail.com>
2021-07-13 00:20:58 +0530

    Old variable declaration types in JS using var in second video

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello world');
+//Old Method to declare variables
+
+var life = 100; // this is the life bar
+
+life = 50;
+
+var name = 'Swaraj'; // this is the name
+
+var checkout = true; // a boolean
+
+var box; // undefined
+
+var abc = null; // null value
+
+object, symbol
+
+
+// New way to create variable using let and const
+
+
+
+
+console.log(box);
